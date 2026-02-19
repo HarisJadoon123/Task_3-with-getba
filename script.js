df2300475cbaaf1105c928a157dedc755567323a
Haris Jadoon <harisjadoo5@gmail.com>
2026-02-19 11:01:00 +0500

    ALL Errors Shows at once Now

--- script.js
+++ script.js
@@ -14,37 +14,71 @@ function submission(e) {
     document.getElementById("error4").innerText = "";
     document.getElementById("error5").innerText = "";
 
-    if (name.value.trim() === "")                                            //Name Validation 
-        document.getElementById("error").innerText = "Please Enter Your Name";
-    else if (/\d/g.test(name.value))
+    let isValid = true;
+
+    if (name.value.trim() === "")                                            //Name Validation
+     {
+         document.getElementById("error").innerText = "Please Enter Your Name";
+         isValid = false
+        }
+    else if (/\d/g.test(name.value)){
         document.getElementById("error").innerText = "Name Only Contain Alphabets";
+        isValid = false
+    }
 
-    else if (email.value.trim() === "")                                      //Emial Validation
+    if (email.value.trim() === "")                                      //Emial Validation
+    {
         document.getElementById("error1").innerText = "Please Enter Your Email Address ";
-    else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value))
+        isValid = false
+    }
+        else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)){
         document.getElementById("error1").innerText = "Invalid Email Address";
+        isValid = false
+    }
 
-    else if (dob.value.trim() === "")                                        //DOB Validation
+    if (dob.value.trim() === "")                                        //DOB Validation
+    {
         document.getElementById("error2").innerText = "Please Select Your Date Of Birth";
+        isValid = false
+    }
 
-    else if (phone.value.trim() === "")                                      //Phone Validation
+    if (phone.value.trim() === "")                                      //Phone Validation
+    {
         document.getElementById("error3").innerText = "Please Enter Your Phone Number";
-
-    else if (phone.value.trim().length > 11)
+        isValid = false;
+    }
+        
+    else if (phone.value.trim().length > 11){
         document.getElementById("error3").innerText = "Phone Number Cannot Exceed 11 digit";
+        isValid = false;
+    }
 
-    else if (!/^\d+$/.test(phone.value))
+    else if (!/^\d+$/.test(phone.value)){
         document.getElementById("error3").innerText = "Phone Number Only Contain Numbers";
+        isValid = false;
+    }
 
-    else if (cnic.value.trim() === "")                                      //CNIC Validation
+    if (cnic.value.trim() === "")                                      //CNIC Validation
+    {
         document.getElementById("error4").innerText = "Please Enter Your CNIC";
-    else if (cnic.value.trim().length > 13)
+        isValid = false;
+    }
+        else if (cnic.value.trim().length > 13){
         document.getElementById("error4").innerText = "CNIC Cannot Exceed 13 digit";
-    else if (!/^\d+$/.test(cnic.value))
+        isValid = false;
+    }
+    else if (!/^\d+$/.test(cnic.value)){
         document.getElementById("error4").innerText = "CNIC Only Contain Numbers";
+        isValid = false;
+    }
 
-        else if (picture.value.trim() === "")                             //Picture Validation
+    if (picture.value.trim() === "")                             //Picture Validation
+    {
         document.getElementById("error5").innerText = "Please Upload Your Profile Picture";
-    else
-        alert("FORM SUBMITTED SUCCESSFULLY")
+        isValid = false;
+    }
+    if(isValid)
+        alert("FORM SUBMITTED SUCCESSFULLY");
 }
+
+
